792508e2463b13c5da14cb32fd194c1e94bfc65d
AppDevIn <teamprojectlive@gmail.com>
2021-01-25 01:20:52 +0800

    Menu in the main program

--- main.cpp
+++ main.cpp
@@ -1,33 +1,59 @@
 #include <iostream>
-#include<string>
+#include <string>
 #include "Trie.cpp"
 
 using namespace std;
 
+void menu();
+void menuSelection(int pick);
+void insertStudent(string name, string studentID);
+void deleteStudent(string studentID);
+void searchStudent(string studentID);
+void prefixSearchStudent(string prefixID);
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[])
+{
 
+    int pick = 1;
 
-    Trie t = Trie();
+    cout << "Welcome to Student Management System" << endl;
 
-    t.insert("jeya", 10);
-    t.insert("runlin", 20);
-    t.insert("runny", 20);
-    t.insert("run", 20);
-    t.insert("zac", 200);
-    t.search("jeya")? cout << "Yes\n" : 
-                         cout << "No\n"; 
+    while (pick != 0)
+    {
+        cout << endl;
+        //fucntion to call the menu
+        menu();
 
-    cout << t.get("jeya") << endl;
-    cout << t.get("zac") << endl;
-
-    t.remove("jeya");
-
-    t.search("jeya")? cout << "Yes\n" : 
-                        cout << "No\n"; 
-
-    t.startsWith("ru");
-    
+        // choose what you want
+        cin >> pick;
+        menuSelection(pick);
+    }
 
     return 0;
+}
+
+void menu()
+{
+    cout << "Student Management System Options" << endl;
+    cout << 1 << ". Insert Student" << endl;
+    cout << 2 << ". Delete Student" << endl;
+    cout << 3 << ". Search Student" << endl;
+    cout << 4 << ". Prefix Search" << endl;
+    cout << "Choose an option:  ";
+}
+
+void menuSelection(int pick)
+{
+    switch (pick)
+    {
+    case 0:
+        exit(0);
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        cout << "Sorry, please choose a valid option.";
+    }
 }
